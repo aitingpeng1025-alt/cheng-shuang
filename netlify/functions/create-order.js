1359afb4d59f949480f7c062c7ac280bc24a6ffe
aitingpeng1025-alt <aitingpeng1025@gmail.com>
2026-06-02 17:59:22 +0800

    Create create-order.js

---
+++ netlify/functions/create-order.js
@@ -0,0 +1,52 @@
+const crypto = require('crypto');
+
+const MERCHANT_ID = '3350784';
+const HASH_KEY = 'LGtbLNctGyKMHCKd';
+const HASH_IV = 'ZfXzRRBdTemplmLf';
+
+function generateCheckMac(params) {
+  const sorted = Object.keys(params).sort().reduce((obj, key) => {
+    obj[key] = params[key];
+    return obj;
+  }, {});
+  let str = 'HashKey=' + HASH_KEY;
+  for (const [k, v] of Object.entries(sorted)) str += '&' + k + '=' + v;
+  str += '&HashIV=' + HASH_IV;
+  str = encodeURIComponent(str)
+    .replace(/%2d/gi,'-').replace(/%5f/gi,'_').replace(/%2e/gi,'.')
+    .replace(/%21/gi,'!').replace(/%2a/gi,'*').replace(/%28/gi,'(').replace(/%29/gi,')').toLowerCase();
+  return crypto.createHash('sha256').update(str).digest('hex').toUpperCase();
+}
+
+exports.handler = async (event) => {
+  if (event.httpMethod !== 'POST') return { statusCode: 405, body: 'Method Not Allowed' };
+  try {
+    const body = JSON.parse(event.body);
+    const { productName, price, payMethod } = body;
+    const now = new Date();
+    const pad = n => String(n).padStart(2,'0');
+    const tradeNo = 'CS' + now.getFullYear() + pad(now.getMonth()+1) + pad(now.getDate()) + pad(now.getHours()) + pad(now.getMinutes()) + pad(now.getSeconds());
+    const tradeDate = now.getFullYear() + '/' + pad(now.getMonth()+1) + '/' + pad(now.getDate()) + ' ' + pad(now.getHours()) + ':' + pad(now.getMinutes()) + ':' + pad(now.getSeconds());
+    const params = {
+      MerchantID: MERCHANT_ID,
+      MerchantTradeNo: tradeNo,
+      MerchantTradeDate: tradeDate,
+      PaymentType: 'aio',
+      TotalAmount: String(price),
+      TradeDesc: 'ChengShuang教材',
+      ItemName: productName,
+      ReturnURL: 'https://cheng-shuang.netlify.app/order-complete.html',
+      ClientBackURL: 'https://cheng-shuang.netlify.app/order-complete.html',
+      ChoosePayment: payMethod,
+      EncryptType: '1',
+    };
+    params.CheckMacValue = generateCheckMac(params);
+    return {
+      statusCode: 200,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ params, action: 'https://payment.ecpay.com.tw/Checkout/Index' })
+    };
+  } catch(err) {
+    return { statusCode: 500, body: JSON.stringify({ error: err.message }) };
+  }
+};
